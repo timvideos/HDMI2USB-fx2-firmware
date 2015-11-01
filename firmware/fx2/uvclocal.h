84456143cf8d3336181b8fb4c273d44b090a85d7
Tim 'mithro' Ansell <mithro@mithis.com>
2015-11-01 13:42:06 +1100

    Moving some defines into the uvclocal.h header.

---
+++ firmware/fx2/uvclocal.h
@@ -0,0 +1,22 @@
+#ifndef UVC_LOCAL_H
+#define UVC_LOCAL_H
+
+#define FRAME_INTERVAL_60FPS 166667	// 60.0fps ==  166666.6 ns
+#define FRAME_INTERVAL_50FPS 200000	// 50.0fps ==  200000.0 ns
+#define FRAME_INTERVAL_30FPS 333333	// 30.0fps ==  333333.3 ns
+#define FRAME_INTERVAL_15FPS 666667	// 15.0fps ==  666666.6 ns
+#define FRAME_INTERVAL_7FPS 1333333	//  7.5fps == 1333333.3 ns
+
+//        .db 0x00,0x20,0x1C,0x00          ;/* Maximum video or still frame size in bytes */ 
+// 1843200
+#define FRAME_SIZE_1280x720 0x1C2000
+
+//        .db 0x00,0x00,0x18,0x00          ;/* Maximum video or still frame size in bytes */
+// 1572864
+#define FRAME_SIZE_1024x768 0x180000
+
+//        .db 0x00,0x00,0x00,0x0E          ;/* Min bit rate bits/s */ 
+// 234881024
+#define BIT_RATE 0xE000000
+
+#endif
