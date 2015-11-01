8f8d373ccba6aa620166e0ca5b2693458000c750
Tim 'mithro' Ansell <mithro@mithis.com>
2015-11-01 13:44:15 +1100

    Making the uvc code more standard compliant.

--- firmware/fx2/uvc.c
+++ firmware/fx2/uvc.c
@@ -8,37 +8,71 @@
  *
  */
 
+#include "cdc-config.h"
+
 #include "uvc.h"
+#include "uvclocal.h"
 
 #include <fx2regs.h>
 #include <fx2macros.h>
 #include <setupdat.h>
 #include <eputils.h>
 #include <delay.h>
+
 #define SYNCDELAY SYNCDELAY4
 
-#include "cdc-config.h"
+//#define UVC_XXX_SIZE 26
+#define UVC_XXX_SIZE (sizeof(struct uvc_streaming_control))
 
-BYTE valuesArray[26]=
-{
-    0x01,0x00,                       /* bmHint : No fixed parameters */
-    0x01,                            /* Use 1st Video format index */
-    0x01,                            /* Use 1st Video frame index */
-    0x2A,0x2C,0x0A,0x00,             /* Desired frame interval in 100ns */
+// FIXME: These are actually stored in the descriptor table!
 
-    0x00,0x00,                       /* Key frame rate in key frame/video frame units */
-    0x00,0x00,                       /* PFrame rate in PFrame / key frame units */
-    0x00,0x00,                       /* Compression quality control */
-    0x00,0x00,                       /* Window size for average bit rate */
-
-    0x05,0x00,                       /* Internal video streaming i/f latency in ms */
-
-    0x00,0x20,0x1C,0x00,            /* Max video frame size in bytes*/
-    0x00,0x04,0x00,0x00              /* No. of bytes device can rx in single payload (1024) */
+//BYTE fps[2][4] = {{0x2A,0x2C,0x0A,0x00},{0x54,0x58,0x14,0x00}}; // 15 ,7
+const DWORD fps[] = {
+	FRAME_INTERVAL_15FPS,
+	FRAME_INTERVAL_7FPS,
+};
+//BYTE frameSize[2][4] = {{0x00,0x00,0x18,0x00},{0x00,0x20,0x1C,0x00}};// Dvi , HDMI
+const DWORD frameSize[] = {
+	FRAME_SIZE_1024x768, // DVI
+	FRAME_SIZE_1280x720, // HDMI
 };
 
-BYTE fps[2][4] = {{0x2A,0x2C,0x0A,0x00},{0x54,0x58,0x14,0x00}}; // 15 ,7
-BYTE frameSize[2][4] = {{0x00,0x00,0x18,0x00},{0x00,0x20,0x1C,0x00}};// Dvi , HDMI
+//BYTE valuesArray[UVC_XXX_SIZE]=
+//{
+//    0x01,0x00,                       /* bmHint : No fixed parameters */
+//    0x01,                            /* Use 1st Video format index */
+//    0x01,                            /* Use 1st Video frame index */
+//    0x2A,0x2C,0x0A,0x00,             /* Desired frame interval in 100ns */
+//
+//    0x00,0x00,                       /* Key frame rate in key frame/video frame units */
+//    0x00,0x00,                       /* PFrame rate in PFrame / key frame units */
+//    0x00,0x00,                       /* Compression quality control */
+//    0x00,0x00,                       /* Window size for average bit rate */
+//
+//    0x05,0x00,                       /* Internal video streaming i/f latency in ms */
+//
+//    0x00,0x20,0x1C,0x00,            /* Max video frame size in bytes*/
+//    0x00,0x04,0x00,0x00              /* No. of bytes device can rx in single payload (1024) */
+//};
+union uvc_streaming_control_array {
+	struct uvc_streaming_control control;
+	BYTE array[sizeof(struct uvc_streaming_control)];
+} valuesArray = {
+	.control = {
+		.bmHint			= UVC_PROBE_BMHINT_dwFrameInterval,
+		.bFormatIndex		= 1,
+		.bFrameIndex		= 1,
+		.dwFrameInterval	= fps[0], /* in 100ns */
+		.wKeyFrameRate		= 0,
+		.wPFrameRate		= 0,
+		.wCompQuality		= 0,
+		.wCompWindowSize	= 0,
+		.wDelay			= 5, /* in ms */
+		.dwMaxVideoFrameSize	= frameSize[0], // in bytes
+		.dwMaxPayloadTransferSize = 1024, // in bytes
+	},
+};
+
 
 BOOL handleUVCCommand(BYTE cmd)
 {
@@ -51,24 +85,28 @@ BOOL handleUVCCommand(BYTE cmd)
             return FALSE;
 
         EP0BCH = 0;
-        EP0BCL = 26;
+        EP0BCL = UVC_XXX_SIZE;
         SYNCDELAY;
         while (EP0CS & bmEPBUSY);
-        while (EP0BCL != 26);
+        while (EP0BCL != UVC_XXX_SIZE);
 
-        valuesArray[2] = EP0BUF[2]; // formate
-        valuesArray[3] = EP0BUF[3]; // frame
+        //valuesArray[2] = EP0BUF[2]; // format index
+	valuesArray.control.bFormatIndex = EP0BUF[2];
+        //valuesArray[3] = EP0BUF[3]; // frame
+	valuesArray.control.bFrameIndex = EP0BUF[3];
 
         // fps
-        valuesArray[4] = fps[EP0BUF[2]-1][0];
-        valuesArray[5] = fps[EP0BUF[2]-1][1];
-        valuesArray[6] = fps[EP0BUF[2]-1][2];
-        valuesArray[7] = fps[EP0BUF[2]-1][3];
+        //valuesArray[4] = fps[EP0BUF[2]-1][0];
+        //valuesArray[5] = fps[EP0BUF[2]-1][1];
+        //valuesArray[6] = fps[EP0BUF[2]-1][2];
+        //valuesArray[7] = fps[EP0BUF[2]-1][3];
+	valuesArray.control.dwFrameInterval = fps[valuesArray.control.bFrameIndex-1];
 
-        valuesArray[18] = frameSize[EP0BUF[3]-1][0];
-        valuesArray[19] = frameSize[EP0BUF[3]-1][1];
-        valuesArray[20] = frameSize[EP0BUF[3]-1][2];
-        valuesArray[21] = frameSize[EP0BUF[3]-1][3];
+        //valuesArray[18] = frameSize[EP0BUF[3]-1][0];
+        //valuesArray[19] = frameSize[EP0BUF[3]-1][1];
+        //valuesArray[20] = frameSize[EP0BUF[3]-1][2];
+        //valuesArray[21] = frameSize[EP0BUF[3]-1][3];
+	valuesArray.control.dwMaxVideoFrameSize = frameSize[valuesArray.control.bFormatIndex-1];
 
         EP0BCH = 0; // ACK
         EP0BCL = 0; // ACK
@@ -79,12 +117,12 @@ BOOL handleUVCCommand(BYTE cmd)
     case UVC_GET_MAX:
         SUDPTRCTL = 0x01;
 
-        for (i = 0; i < 26; i++)
-            EP0BUF[i] = valuesArray[i];
+        for (i = 0; i < UVC_XXX_SIZE; i++)
+            EP0BUF[i] = valuesArray.array[i];
 
         EP0BCH = 0x00;
         SYNCDELAY;
-        EP0BCL = 26;
+        EP0BCL = UVC_XXX_SIZE;
         return TRUE;
 
         // FIXME: What do these do????
