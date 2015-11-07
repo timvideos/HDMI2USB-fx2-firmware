aee76bf26526fc60294818d80a08c6850f2e9b26
Tim 'mithro' Ansell <mithro@mithis.com>
2015-11-08 02:26:47 +1100

    Cleaning up a little.

--- firmware/fx2/uvc.c
+++ firmware/fx2/uvc.c
@@ -25,35 +25,15 @@
 #define UVC_XXX_SIZE (sizeof(struct uvc_streaming_control))
 
 // FIXME: These are actually stored in the descriptor table!
-
-//BYTE fps[2][4] = {{0x2A,0x2C,0x0A,0x00},{0x54,0x58,0x14,0x00}}; // 15 ,7
 const DWORD fps[] = {
 	FRAME_INTERVAL_15FPS,
 	FRAME_INTERVAL_7FPS,
 };
-//BYTE frameSize[2][4] = {{0x00,0x00,0x18,0x00},{0x00,0x20,0x1C,0x00}};// Dvi , HDMI
 const DWORD frameSize[] = {
 	FRAME_SIZE_1024x768, // DVI
 	FRAME_SIZE_1280x720, // HDMI
 };
 
-//BYTE valuesArray[UVC_XXX_SIZE]=
-//{
-//    0x01,0x00,                       /* bmHint : No fixed parameters */
-//    0x01,                            /* Use 1st Video format index */
-//    0x01,                            /* Use 1st Video frame index */
-//    0x2A,0x2C,0x0A,0x00,             /* Desired frame interval in 100ns */
-//
-//    0x00,0x00,                       /* Key frame rate in key frame/video frame units */
-//    0x00,0x00,                       /* PFrame rate in PFrame / key frame units */
-//    0x00,0x00,                       /* Compression quality control */
-//    0x00,0x00,                       /* Window size for average bit rate */
-//
-//    0x05,0x00,                       /* Internal video streaming i/f latency in ms */
-//
-//    0x00,0x20,0x1C,0x00,            /* Max video frame size in bytes*/
-//    0x00,0x04,0x00,0x00              /* No. of bytes device can rx in single payload (1024) */
-//};
 union uvc_streaming_control_array {
 	struct uvc_streaming_control control;
 	BYTE array[sizeof(struct uvc_streaming_control)];
@@ -90,22 +70,12 @@ BOOL handleUVCCommand(BYTE cmd)
         while (EP0CS & bmEPBUSY);
         while (EP0BCL != UVC_XXX_SIZE);
 
-        //valuesArray[2] = EP0BUF[2]; // format index
-	valuesArray.control.bFormatIndex = EP0BUF[2];
-        //valuesArray[3] = EP0BUF[3]; // frame
-	valuesArray.control.bFrameIndex = EP0BUF[3];
-
         // fps
-        //valuesArray[4] = fps[EP0BUF[2]-1][0];
-        //valuesArray[5] = fps[EP0BUF[2]-1][1];
-        //valuesArray[6] = fps[EP0BUF[2]-1][2];
-        //valuesArray[7] = fps[EP0BUF[2]-1][3];
+	valuesArray.control.bFormatIndex = EP0BUF[2];
 	valuesArray.control.dwFrameInterval = fps[valuesArray.control.bFrameIndex-1];
 
-        //valuesArray[18] = frameSize[EP0BUF[3]-1][0];
-        //valuesArray[19] = frameSize[EP0BUF[3]-1][1];
-        //valuesArray[20] = frameSize[EP0BUF[3]-1][2];
-        //valuesArray[21] = frameSize[EP0BUF[3]-1][3];
+	// frame size
+	valuesArray.control.bFrameIndex = EP0BUF[3];
 	valuesArray.control.dwMaxVideoFrameSize = frameSize[valuesArray.control.bFormatIndex-1];
 
         EP0BCH = 0; // ACK
@@ -142,8 +112,6 @@ BOOL handleUVCCommand(BYTE cmd)
 }
 
 BYTE   Configuration;      // Current configuration
-BYTE   AlternateSetting = 0;   // Alternate settings
-
 BYTE handle_get_configuration()
 {
     return Configuration;
@@ -155,6 +123,7 @@ BOOL handle_set_configuration(BYTE cfg)
     return TRUE;
 }
 
+BYTE   AlternateSetting = 0;   // Alternate settings
 BOOL handle_get_interface(BYTE ifc, BYTE* alt_ifc)
 {
 
@@ -169,7 +138,7 @@ BOOL handle_set_interface(BYTE ifc, BYTE alt_ifc)
 {
     AlternateSetting = SETUPDAT[2];
 
-	if (ifc==0&&alt_ifc==0) {
+	if (ifc==0 && alt_ifc==0) {
 		// SEE TRM 2.3.7
 		// reset toggles
 		CDC_H2D_RESET(TOGGLE);
