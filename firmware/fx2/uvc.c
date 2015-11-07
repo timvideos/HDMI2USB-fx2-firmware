89acf0ab0ab472fa3a3dfeb14b62fd62661a737d
Tim 'mithro' Ansell <mithro@mithis.com>
2015-11-08 02:33:45 +1100

    Reformatting the file.

--- firmware/fx2/uvc.c
+++ firmware/fx2/uvc.c
@@ -56,87 +56,87 @@ union uvc_streaming_control_array {
 
 BOOL handleUVCCommand(BYTE cmd)
 {
-    int i;
+	int i;
 
-    switch(cmd) {
-    case CLEAR_FEATURE:
-        // FIXME: WTF is 0x21 !?
-        if (SETUPDAT[0] != 0x21)
-            return FALSE;
+	switch(cmd) {
+	case CLEAR_FEATURE:
+		// FIXME: WTF is 0x21 !?
+		if (SETUPDAT[0] != 0x21)
+			return FALSE;
 
-        EP0BCH = 0;
-        EP0BCL = UVC_XXX_SIZE;
-        SYNCDELAY;
-        while (EP0CS & bmEPBUSY);
-        while (EP0BCL != UVC_XXX_SIZE);
+		EP0BCH = 0;
+		EP0BCL = UVC_XXX_SIZE;
+		SYNCDELAY;
+		while (EP0CS & bmEPBUSY);
+		while (EP0BCL != UVC_XXX_SIZE);
 
-        // fps
-	valuesArray.control.bFormatIndex = EP0BUF[2];
-	valuesArray.control.dwFrameInterval = fps[valuesArray.control.bFrameIndex-1];
+		// fps
+		valuesArray.control.bFormatIndex = EP0BUF[2];
+		valuesArray.control.dwFrameInterval = fps[valuesArray.control.bFrameIndex-1];
 
-	// frame size
-	valuesArray.control.bFrameIndex = EP0BUF[3];
-	valuesArray.control.dwMaxVideoFrameSize = frameSize[valuesArray.control.bFormatIndex-1];
+		// frame size
+		valuesArray.control.bFrameIndex = EP0BUF[3];
+		valuesArray.control.dwMaxVideoFrameSize = frameSize[valuesArray.control.bFormatIndex-1];
 
-        EP0BCH = 0; // ACK
-        EP0BCL = 0; // ACK
-        return TRUE;
+		EP0BCH = 0; // ACK
+		EP0BCL = 0; // ACK
+		return TRUE;
 
-    case UVC_GET_CUR:
-    case UVC_GET_MIN:
-    case UVC_GET_MAX:
-        SUDPTRCTL = 0x01;
+	case UVC_GET_CUR:
+	case UVC_GET_MIN:
+	case UVC_GET_MAX:
+		SUDPTRCTL = 0x01;
 
-        for (i = 0; i < UVC_XXX_SIZE; i++)
-            EP0BUF[i] = valuesArray.array[i];
+		for (i = 0; i < UVC_XXX_SIZE; i++)
+			EP0BUF[i] = valuesArray.array[i];
 
-        EP0BCH = 0x00;
-        SYNCDELAY;
-        EP0BCL = UVC_XXX_SIZE;
-        return TRUE;
+		EP0BCH = 0x00;
+		SYNCDELAY;
+		EP0BCL = UVC_XXX_SIZE;
+		return TRUE;
 
-        // FIXME: What do these do????
-        // case UVC_SET_CUR:
-        // case UVC_GET_RES:
-        // case UVC_GET_LEN:
-        // case UVC_GET_INFO:
+		// FIXME: What do these do????
+		// case UVC_SET_CUR:
+		// case UVC_GET_RES:
+		// case UVC_GET_LEN:
+		// case UVC_GET_INFO:
 
-        // case UVC_GET_DEF:
-        // FIXME: Missing this case causes the following errors
-        // uvcvideo: UVC non compliance - GET_DEF(PROBE) not supported. Enabling workaround.
-        // Unhandled Vendor Command: 87
+		// case UVC_GET_DEF:
+		// FIXME: Missing this case causes the following errors
+		// uvcvideo: UVC non compliance - GET_DEF(PROBE) not supported. Enabling workaround.
+		// Unhandled Vendor Command: 87
 
-    default:
-        return FALSE;
-    }
+	default:
+		return FALSE;
+	}
 }
 
-BYTE   Configuration;      // Current configuration
+BYTE Configuration;	  // Current configuration
 BYTE handle_get_configuration()
 {
-    return Configuration;
+	return Configuration;
 }
 
 BOOL handle_set_configuration(BYTE cfg)
 {
-    Configuration = SETUPDAT[2];   //cfg;
-    return TRUE;
+	Configuration = SETUPDAT[2];   //cfg;
+	return TRUE;
 }
 
-BYTE   AlternateSetting = 0;   // Alternate settings
+BYTE AlternateSetting = 0;   // Alternate settings
 BOOL handle_get_interface(BYTE ifc, BYTE* alt_ifc)
 {
 
-    *alt_ifc = AlternateSetting;
-    //EP0BUF[0] = AlternateSetting;
-    //EP0BCH = 0;
-    //EP0BCL = 1;
-    return TRUE;
+	*alt_ifc = AlternateSetting;
+	//EP0BUF[0] = AlternateSetting;
+	//EP0BCH = 0;
+	//EP0BCL = 1;
+	return TRUE;
 }
 
 BOOL handle_set_interface(BYTE ifc, BYTE alt_ifc)
 {
-    AlternateSetting = SETUPDAT[2];
+	AlternateSetting = SETUPDAT[2];
 
 	if (ifc==0 && alt_ifc==0) {
 		// SEE TRM 2.3.7
@@ -152,17 +152,17 @@ BOOL handle_set_interface(BYTE ifc, BYTE alt_ifc)
 		CDC_D2H_RESET(FIFO);
 	}
 
-    if (AlternateSetting == 1) {
-        // reset UVC fifo
-        SYNCDELAY; FIFORESET = 0x80;
-        SYNCDELAY; FIFORESET = 0x06;
-        SYNCDELAY; FIFORESET = 0x00;
-    }
+	if (AlternateSetting == 1) {
+		// reset UVC fifo
+		SYNCDELAY; FIFORESET = 0x80;
+		SYNCDELAY; FIFORESET = 0x06;
+		SYNCDELAY; FIFORESET = 0x00;
+	}
 
-    return TRUE;
+	return TRUE;
 }
 
 
 BOOL handle_get_descriptor() {
-    return FALSE;
+	return FALSE;
 }
