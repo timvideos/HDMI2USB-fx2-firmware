cd56140366facca34668ebbab6be8edae685ca3d
Tim 'mithro' Ansell <mithro@mithis.com>
2015-11-13 18:54:36 +1100

    Starting to get the UVC control stuff working.

--- firmware/fx2/uvc.h
+++ firmware/fx2/uvc.h
@@ -43,12 +43,15 @@ struct uvc_status_packet_from_stream_interface {
 // Video Control / Video Streaming Requests
 
 
+#define UVC_REQUEST_TYPE_MASK		0x0f
+#define UVC_REQUEST_TYPE_CONTROL	(1 << 0)	// 4.2 VideoControl Requests
+#define UVC_REQUEST_TYPE_STREAM		(1 << 1)	// 4.3 VideoStreaming Requests
 
 struct uvc_control_request {
 	// D7 == Get 1 or Set 0
 	// D6..5 == 01 -> Class specific request
 	// D4..0 == 00001 -> Video Control/Video Streaming Interfaces or Video Function
-	// D4..0 == 00010 -> Vide Data endpoint (of Video Streaming Interface)
+	// D4..0 == 00010 -> Video Data endpoint (of Video Streaming Interface)
 	BYTE bmRequestType;
 	BYTE bRequest;
 
@@ -61,14 +64,16 @@ struct uvc_control_request {
 	// Terminal, the control pipe must indicate a protocol STALL.
 
 	// wValue == Control Selector in high byte
-	WORD wValue;
+	union {
+		BYTE bControl;
+		BYTE bZero;
+	} wValue;
 
-	// Unit/Terminal ID and Interface
-	// --or--
-	// Endpoint
 	//WORD wIndex;
-	BYTE bEntityId;
-	BYTE bEndpoint;
+	struct {
+		BYTE bUnitId;
+		BYTE bInterface;
+	} wIndex;
 
 	// Length of parameter block
 	WORD wLength;
@@ -80,131 +85,117 @@ struct uvc_control_request {
 
 // Power Mode Control
 // VC_VIDEO_POWER_MODE_CONTROL
-// SET_CUR, GET_CUR, GET_INFO
-struct uvc_video_power_mode_control {
-	
+
+
+// 4.2 VideoControl Requests
+// =======================================================================
+// 4.2.1 Interface Control Requests
+
+// 4.2.1.1 Power Mode Control
+// -----------------------------------------------------------------------
+// UVC_VC_VIDEO_POWER_MODE_CONTROL
+// Supports: SET_CUR, GET_CUR, GET_INFO
+// wLength: 1 byte
+#define UVC_VC_POWER_MODE_CONTROL_DATA_MODE_MASK	0x07 // 0b0000111
+#define UVC_VC_POWER_MODE_CONTROL_DATA_MODE_FULL_POWER  0x00
+#define UVC_VC_POWER_MODE_CONTROL_DATA_MODE_DEPENDENT	0x01
+
+// Read only
+#define UVC_VC_POWER_MODE_CONTROL_SUPPORTS_DEPENDENT	(1 << 4)
+#define UVC_VC_POWER_MODE_CONTROL_POWERED_USB		(1 << 5)
+#define UVC_VC_POWER_MODE_CONTROL_POWERED_BATTERY	(1 << 6)
+#define UVC_VC_POWER_MODE_CONTROL_POWERED_AC		(1 << 7)
+
+struct uvc_vc_data_power_mode_control {
+	BYTE bmDevicePowerMode;
 };
 
-/*
-Sets the streaming interface Probe state. This is the attribute used for
-stream parameter negotiation.
-*/
-//#define UVC_SET_CUR                                     (0x01)
-
-// GET
-
-
-enum uvc_get_values {
-	UVC_CUR = 1,
-	UVC_MIN,
-	UVC_MAX,
-	UVC_RES,	// Resolution attribute
-	UVC_LEN,	// Data length
-	UVC_INFO,	// Information attribute
-	UVC_DEF,	// Default
+// 4.2.1.2 Request Error Code Control
+// -----------------------------------------------------------------------
+// UVC_VC_REQUEST_ERROR_CODE_CONTROL
+// Supports: GET_CUR, GET_INFO
+// wLength: 1 byte
+enum {
+	ERROR_CODE_NONE = 0,
+	ERROR_CODE_NOT_READY,
+	ERROR_CODE_WRONG_STATE,
+	ERROR_CODE_POWER,
+	ERROR_CODE_OUT_OF_RANGE,
+	ERROR_CODE_INVALID_UNIT,
+	ERROR_CODE_INVALID_CONTROL,
+	ERROR_CODE_INVALID_REQUEST,
+	ERROR_CODE_INVALID_VALUE,
+	ERROR_CODE_UNKNOWN = 0xFF,
+} bmRequestControlErrorCodeType;
+struct uvc_vc_data_error_code_control {
+	bmRequestControlErrorCodeType bmRequestErrorCode;
 };
 
-#define UVC_GET_XXX_ONE					(0x80)
-#define UVC_GET_XXX_ALL					(0x90)
+// 4.2.2 Unit and Terminal Control Requests
+// 4.2.2.1 Camera Terminal Control Requests
+// -----------------------------------------------------------------------
+// We don't implement most of the camera controls, not being a camera.
 
-
-/*
-Returns the current state of the streaming interface. All supported fields set
-to zero will be returned with an acceptable negotiated value.  Prior to the
-initial SET_CUR operation, the GET_CUR state is undefined. This request shall
-stall in case of negotiation failure.
-*/
-#define UVC_GET_CUR                                     (UVC_GET_XXX_ONE|UVC_CUR)
-#define UVC_GET_CUR_ALL					(UVC_GET_XXX_ALL|UVC_CUR)
-
-/* Returns the minimum value for negotiated fields. */
-#define UVC_GET_MIN                                     (UVC_GET_XXX_ONE|UVC_MIN)
-#define UVC_GET_MIN_ALL                                 (UVC_GET_XXX_ALL|UVC_MIN)
-
-/* Returns the maximum value for negotiated fields. */
-#define UVC_GET_MAX                                     (UVC_GET_XXX_ONE|UVC_MAX)
-#define UVC_GET_MAX_ALL                                 (UVC_GET_XXX_ALL|UVC_MAX)
-
-/* 
-Return the resolution of each supported field in the Probe/Commit data
-structure. 
-*/
-#define UVC_GET_RES                                     (UVC_GET_XXX_ONE|UVC_RES)
-#define UVC_GET_RES_ALL                                 (UVC_GET_XXX_ALL|UVC_RES)
-
-/*
-Returns the length of the Commit or Probe data structure.
-*/
-#define UVC_GET_LEN                                     (UVC_GET_XXX_ONE|UVC_LEN)
-#define UVC_GET_LEN_ALL                                 (UVC_GET_XXX_ALL|UVC_LEN)
-
-/*
-Queries the capabilities and status of the Control. The value returned for this
-request shall have bits D0 and D1 each set to one (1), and the remaining bits
-set to zero (0) (see section 4.1.2, “Get Request”).
-*/
-#define UVC_GET_INFO                                    (UVC_GET_XXX_ONE|UVC_INFO)
-#define UVC_GET_INFO_ALL                                (UVC_GET_XXX_ALL|UVC_INFO)
-
-// UVC_GET_INFO
-// wLength == 1
-#define UVC_GET_INFO_SUPPORTS_GET			(1 << 0)
-#define UVC_GET_INFO_SUPPORTS_SET			(1 << 1)
-#define UVC_GET_INFO_DISABLED_AUTO			(1 << 2)
-#define UVC_GET_INFO_AUTOUPDATE				(1 << 3)
-#define UVC_GET_INFO_ASYNC				(1 << 4)
-#define UVC_GET_INFO_DISABLED_INCOMPAT			(1 << 5)
-struct uvc_control_request_get_info_data {
-	BYTE	supportsGet	: 1;
-	BYTE	supportsSet	: 1;
-	BYTE	disabledAuto	: 1; // Disabled due to automatic mode
-	BYTE	autoupdate	: 1;
-	BYTE	async		: 1;
-	BYTE	disabledIncompat: 1; // Disabled due to incompatibility
+// 4.2.2.1.1 Scanning Mode Control
+// -----------------------------------------------------------------------
+// UVC_CT_SCANNING_MODE_CONTROL
+// Supports: SET_CUR, GET_CUR, GET_INFO
+// wLength: 1 byte
+#define UVC_VC_SCANNING_MODE_CONTROL_INTERLACED		0
+#define UVC_VC_SCANNING_MODE_CONTROL_PROGRESSIVE	1
+struct uvc_vc_data_scanning_mode_control {
+	BYTE bScanningMode;
 };
 
+// 4.2.2.2 Selector Unit Control Requests
+// -----------------------------------------------------------------------
+// UVC_SU_INPUT_SELECT_CONTROL
+// Supports: SET_CUR, GET_CUR, GET_MIN, GET_MAX, GET_RES, GET_INFO
+// wLength: 1 byte
 
-/* 
-Returns the default value for the negotiated fields.
-*/
-#define UVC_GET_DEF                                     (UVC_GET_XXX_ONE|UVC_DEF)
-#define UVC_GET_DEF_ALL                                 (UVC_GET_XXX_ALL|UVC_DEF)
+// 4.2.2.3 Processing Unit Control Requests
+// -----------------------------------------------------------------------
+// We don't implement most of the processing unit controls, not being a
+// camera.
 
-/* 2.4.2.2. Status Packet Type */
-#define UVC_STATUS_TYPE_CONTROL				1
-#define UVC_STATUS_TYPE_STREAMING			2
+// 4.2.2.3.6 Power Line Frequency Control
+// -----------------------------------------------------------------------
+// UVC_PU_POWER_LINE_FREQUENCY_CONTROL
+// Supports: SET_CUR, GET_CUR, GET_INFO, GET_DEF
+// wLength: 1 byte
+#define UVC_PU_POWER_LINE_FREQUENCY_CONTROL_DISABLED	0
+#define UVC_PU_POWER_LINE_FREQUENCY_CONTROL_50HZ	1
+#define UVC_PU_POWER_LINE_FREQUENCY_CONTROL_60HZ	2
+#define UVC_PU_POWER_LINE_FREQUENCY_CONTROL_AUTO	3
 
-/* 4.3.1.1. Video Probe and Commit Controls */
-struct uvc_streaming_control {
-	WORD  bmHint;
-	BYTE  bFormatIndex;
-	BYTE  bFrameIndex;
-	DWORD dwFrameInterval;
-	WORD  wKeyFrameRate;
-	WORD  wPFrameRate;
-	WORD  wCompQuality;
-	WORD  wCompWindowSize;
+// 4.2.2.3.19 Analog Video Lock Status Control
+// -----------------------------------------------------------------------
+// UVC_PU_ANALOG_LOCK_STATUS_CONTROL
+// GET_CUR, GET_INFO
+// wLength: 1 byte
+#define UVC_PU_ANALOG_LOCK_STATUS_CONTROL_SIGNAL	0
+#define UVC_PU_ANALOG_LOCK_STATUS_CONTROL_NOSIGNAL	1
 
-	WORD  wDelay;
+// 4.2.2.4 Encoding Units
+// -----------------------------------------------------------------------
+// Doesn't seem to be supported under Linux? Complicated!?
 
-	DWORD dwMaxVideoFrameSize;
-	DWORD dwMaxPayloadTransferSize;
-/*
-	DWORD dwClockFrequency;
-	BYTE  bmFramingInfo;
-	BYTE  bPreferedVersion;
-	BYTE  bMinVersion;
-	BYTE  bMaxVersion;
- */
-/*
-	BYTE  bUsage;
-	BYTE  bBitDepthLuma;
-	BYTE  bmSettings;
-	BYTE  bMaxNumberOfRefFramesPlus1;
-	BYTE  bmRateControlModes;
-	BYTE  bmLayoutPerStream;
-*/
-};
+// 4.2.2.5 Extension Unit Control Requests
+// -----------------------------------------------------------------------
+// Not sure what to do here...
+// GET_LEN will have a wLength == 2!?
+
+// 4.3 VideoStreaming Requests
+// 4.3.1 Interface Control Requests
+// =======================================================================
+
+// 4.3.1.1. Video Probe and Commit Controls
+// -----------------------------------------------------------------------
+// UVC_VS_PROBE_CONTROL
+// Supports: SET_CUR, GET_CUR, GET_MIN, GET_MAX, GET_RES, GET_DEF, GET_LEN, GET_INFO
+// UVC_VS_COMMIT_CONTROL
+// Supports: SET_CUR, GET_CUR, GET_LEN, GET_INFO
+// wLength: sizeof(uvc_vs_control_data) bytes
 
 
 #define UVC_PROBE_BMHINT_wCompWindowSize	(1 << 4)
@@ -213,15 +204,129 @@ struct uvc_streaming_control {
 #define UVC_PROBE_BMHINT_wKeyFrameRate		(1 << 1)
 #define	UVC_PROBE_BMHINT_dwFrameInterval	(1 << 0)
 
+struct uvc_vs_control_data_v10 {
+	WORD  bmHint;
+
+	BYTE  bFormatIndex;		// Index to descriptor values, 1 == first value
+	BYTE  bFrameIndex;
+	DWORD dwFrameInterval;		// 100 ns
+
+
+	WORD  wKeyFrameRate;
+	WORD  wPFrameRate;
+	WORD  wCompQuality;		// 1 == lowest, 10000 == highest
+	WORD  wCompWindowSize;
+
+	WORD  wDelay;			// in ms 	(always device)
+
+	DWORD dwMaxVideoFrameSize;	// in bytes	(always device)
+
+	// Set MaxPayloadTransferSize fields to zero
+	// Set MaxPayloadTransferSize field to current alternate interface max packet size
+
+	DWORD dwMaxPayloadTransferSize; // in bytes	(???)
+};
+struct uvc_vs_control_data_v11 {
+	WORD  bmHint;
+
+	BYTE  bFormatIndex;		// Index to descriptor values, 1 == first value
+	BYTE  bFrameIndex;
+	DWORD dwFrameInterval;		// 100 ns
+
+	WORD  wKeyFrameRate;
+	WORD  wPFrameRate;
+	WORD  wCompQuality;		// 1 == lowest, 10000 == highest
+	WORD  wCompWindowSize;
+
+	WORD  wDelay;			// in ms
+
+	DWORD dwMaxVideoFrameSize;	// in bytes
+	DWORD dwMaxPayloadTransferSize; // in bytes
+
+	// New in v1.1
+	DWORD dwClockFrequency;		// in Hz
+	BYTE  bmFramingInfo;		// Ignored for MJPEG/Uncompressed/DV
+	BYTE  bPreferedVersion;		// ??? -- bNumFormats
+	BYTE  bMinVersion;		// ???
+	BYTE  bMaxVersion;		// ???
+};
+
+#define UVC_PROBE_PAYLOAD_MJPEG_V10		0
+#define UVC_PROBE_PAYLOAD_MJPEG_V11		1
+#define UVC_PROBE_PAYLOAD_MJPEG_V15		5
+
+struct uvc_vs_control_data_15 {
+	WORD  bmHint;
+
+	BYTE  bFormatIndex;		// Index to descriptor values, 1 == first value
+	BYTE  bFrameIndex;
+
+	DWORD dwFrameInterval;		// 100 ns
+	WORD  wKeyFrameRate;
+	WORD  wPFrameRate;
+	WORD  wCompQuality;		// 1 == lowest, 10000 == highest
+	WORD  wCompWindowSize;
+
+	WORD  wDelay;			// in ms
+
+	DWORD dwMaxVideoFrameSize;	// in bytes
+	DWORD dwMaxPayloadTransferSize; // in bytes
+
+	// New in v1.1
+	DWORD dwClockFrequency;		// in Hz
+	BYTE  bmFramingInfo;		// Ignored for MJPEG/Uncompressed/DV
+	BYTE  bPreferedVersion;		// ??? -- bNumFormats
+	BYTE  bMinVersion;		// ???
+	BYTE  bMaxVersion;		// ???
+
+	// New in v1.5
+	BYTE  bUsage;
+	BYTE  bBitDepthLuma;
+	BYTE  bmSettings;
+	BYTE  bMaxNumberOfRefFramesPlus1;
+	BYTE  bmRateControlModes;
+	BYTE  bmLayoutPerStream;
+};
+
+// size = stream->dev->uvc_version >= 0x0110 ? 34 : 26;
+// 26 bytes for UVC1.0
+// 34 bytes for UVC1.1 and above...
+// 48 bytes for UVC1.5
+BUILD_BUG_ON(sizeof(uvc_vs_control_data_v10) != 26); // 0x1a
+BUILD_BUG_ON(sizeof(uvc_vs_control_data_v11) != 34); // 0x22
+BUILD_BUG_ON(sizeof(uvc_vs_control_data_v15) != 48); // 0x30
+
+
+// 4.3.1.7 Stream Error Code Control
+// -----------------------------------------------------------------------
+// UVC_VS_STREAM_ERROR_CODE_CONTROL
+// Supports: GET_CUR, GET_INFO
+// wLength: 1 byte
+#define UVC_VS_STREAM_ERROR_CODE_CONTROL_
+enum {
+	ERROR_CODE_NONE = 0,
+	ERROR_CODE_PROTECTED_CONTENT,
+	ERROR_CODE_INPUT_UNDERRUN,
+	ERROR_CODE_DISCONTINUITY,
+	ERROR_CODE_OUTPUT_UNDERRUN,
+	ERROR_CODE_OUTPUT_OVERRUN,
+	ERROR_CODE_FORMAT_CHANGE,
+	ERROR_CODE_STILL_CAPTURE_ERROR,
+	ERROR_CODE_UNKNOWN,
+} bmRequestStreamErrorCodeType;
+
+// =======================================================================
 
 BOOL handleUVCCommand(BYTE cmd);
 
+/* 2.4.2.2. Status Packet Type */
+#define UVC_STATUS_TYPE_CONTROL				1
+#define UVC_STATUS_TYPE_STREAMING			2
 
 struct uvc_payload_header {
 	BYTE bHeaderLength;
 	BYTE bmHeaderInfo;
 };
-
 
 
 #endif // UVC_H
