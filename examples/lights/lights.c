31b386950c01032ff40becd746a406e6cd88f687
Kyle Robertze <krobbertze@gmail.com>
2017-07-08 12:03:01 +0200

    examples/lights: Add atlys support

--- examples/lights/lights.c
+++ examples/lights/lights.c
@@ -26,63 +26,99 @@ void init_lights(void) {
     // Set pins to port mode instead of GPIF master/slave mode
     IFCONFIG &= ~(bmIFCFGMASK);
     // No alternate functions
+#ifdef BOARD_atlys
+    PORTDCFG = 0x00;
 #ifdef BOARD_fx2
     PORTACFG = 0x00;
-#endif
+#else
 #ifdef BOARD_opsis
     PORTECFG = 0x00;
-#endif
+#endif // BOARD_opsis
+#endif // BOARD_fx2
+#endif // BOARD_atlys
 }
 
+// d2 is the LED labelled D1 in the Atlys set to the input transfer colour
 // d1 is the LED labelled D1 on the FX2LP CY7C68013A mini-board
 void d1_on(void) {
+#ifdef BOARD_atlys
+    // Set pin D0 as output
+    OED |= bmBIT0;
+    // Set D0
+    IOD &= ~bmBIT0;
+#else
 #ifdef BOARD_fx2
     // Set pin A0 as output
     OEA |= bmBIT0;
     // Set A0
     IOA &= ~bmBIT0;
 #endif // Opsis D1 not connected to FX2
+#endif // BOARD_atlys
 }
 
+// d2 is the LED labelled D1 in the Atlys set to the output transfer colour
 // d2 is the LED labelled D2 on the FX2LP CY7C68013A mini-board
 // d2 is the LED labelled D2 on the Numato Opsis
 void d2_on(void) {
+#ifdef BOARD_atlys
+    // Set pin D1 as output
+    OED |= bmBIT1;
+    // Set D1
+    IOD &= ~bmBIT1;
+#else
 #ifdef BOARD_fx2
     // Set pin A1 as output
     OEA |= bmBIT1;
-    // Set A1 high
+    // Set A1
     IOA &= ~bmBIT1;
-#endif
+#else
 #ifdef BOARD_opsis
     // Set pin E5 as output
     OEE |= bmBIT5;
     // Set E5
     IOE &= ~bmBIT5;
-#endif
+#endif // BOARD_opsis
+#endif // BOARD_fx2
+#endif // BOARD_atlys
 }
 
 void d1_off(void) {
+#ifdef BOARD_atlys
+    // Set pin D0 as output
+    OED |= bmBIT0;
+    // Clear D0
+    IOD |= bmBIT0;
+#else
 #ifdef BOARD_fx2
     // Set pin A0 as output
     OEA |= bmBIT0;
     // Clear A0
     IOA |= bmBIT0;
 #endif // Opsis D1 not connected to FX2
+#endif // BOARD_atlys
 }
 
 void d2_off(void) {
+#ifdef BOARD_atlys
+    // Set pin D1 as output
+    OED |= bmBIT1;
+    // Clear D1
+    IOD |= bmBIT1;
+#else
 #ifdef BOARD_fx2
     // Set pin A1 as output
     OEA |= bmBIT1;
     // Clear A1
     IOA |= bmBIT1;
-#endif
+#else
 #ifdef BOARD_opsis
     // Set pin E5 as output
     OEE |= bmBIT5;
     // Clear E5
     IOE |= bmBIT5;
-#endif
+#endif // BOARD_opsis
+#endif // BOARD_fx2
+#endif // BOARD_atlys
 }
 
 void main(void) {
