99d7594a6f86361d96ae50dd37894d0182667ec6
Kyle Robertze <krobbertze@gmail.com>
2017-07-12 11:05:34 +0200

    examples/lights: Add filename to file comment

--- examples/lights/lights.c
+++ examples/lights/lights.c
@@ -16,7 +16,7 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-/** \file
+/** \file lights.c
  * Example firmware that flashes LEDs.
  *
  * On the Atlys this is LD14, on the FX2 miniboard D1 and D2 and on the 
