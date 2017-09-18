f9c108500657c05366754f5b527947e98e2bce5e
Tim 'mithro' Ansell <mithro@mithis.com>
2017-09-17 18:18:31 -0700

    Fixing detached head issue on Travis.
    For some reason the currently fetched commit doesn't match
    TRAVIS_COMMIT, so explicitly fetch the commit from the origin.

--- common/version_data.sh
+++ common/version_data.sh
@@ -9,8 +9,16 @@ DID=0x$3
 
 git fetch --unshallow && git fetch --tags
 if [ z"$TRAVIS_BRANCH" != z ]; then
-	echo "Fixing detached head"
+	TRAVIS_COMMIT_ACTUAL=$(git log --pretty=format:'%H' -n 1)
+	echo "Fixing detached head (current $TRAVIS_COMMIT_ACTUAL -> $TRAVIS_COMMIT)"
+	echo "---------------------------------------------"
+	git log -n 5 --graph
+	echo "---------------------------------------------"
+	git fetch origin $TRAVIS_COMMIT
 	git branch -v
+	echo "---------------------------------------------"
+	git log -n 5 --graph
+	echo "---------------------------------------------"
 	git branch -D $TRAVIS_BRANCH || true
 	git checkout $TRAVIS_COMMIT -b $TRAVIS_BRANCH
 	git branch -v
