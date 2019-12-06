be6f92938cc22fde10ce205a3b8fbe4f8a677bdc
Jędrzej Boczar <jboczar@antmicro.com>
2019-12-06 16:22:32 +0100

    move common makefile code to spearate file

---
+++ common/common.mk
@@ -0,0 +1,39 @@
+# use conda enviorment if it exists
+MAKEFILE_PATH := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+export PATH := $(MAKEFILE_PATH)../conda/bin:$(PATH)
+
+MODEL ?= small
+BOARD ?= opsis
+SYNCDELAYLEN ?= 4
+
+CFLAGS =
+CFLAGS += --std-sdcc99
+CFLAGS += -Wa"-p"
+CFLAGS += --xram-size 0x0200
+CFLAGS += -DSYNCDELAYLEN=$(SYNCDELAYLEN)
+
+# should to override previously set flags
+FLAGS ?=
+CFLAGS += $(FLAGS)
+
+# set USB IDs depending on board
+ifeq (${BOARD},atlys)
+	VID := 1D50
+	PID := 60B7
+	DID := 0002
+else
+	ifeq (${BOARD},opsis)
+		VID := 2A19
+		PID := 5442
+		DID := 0002
+	else
+		$(error "Unknown board type '$(BOARD)'")
+	endif
+endif
+
+CFLAGS += -DVID=0x$(VID) -DPID=0x$(PID) -DDID=0x$(DID)
+
+LIBFX2DIR ?= ../third_party/libfx2
+# variable required by libfx2 build system
+LIBFX2 = $(LIBFX2DIR)/firmware/library
+include $(LIBFX2)/fx2rules.mk
